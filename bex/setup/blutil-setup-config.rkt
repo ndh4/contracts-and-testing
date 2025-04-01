cca1583f731cd18dd0db8760e8d24d533fc33dc4
ndh4 <ndh4@rice.edu>
2025-04-01 13:57:05 -0500

    Use forked gtp-benchmarks repo

--- bex/setup/blutil-setup-config.rkt
+++ bex/setup/blutil-setup-config.rkt
@@ -3,7 +3,7 @@
 (provide (all-defined-out))
 
 (define expected-TR-branch "transient-blame2")
-(define gtp-repo-url "https://github.com/llazarek/gtp-benchmarks.git")
+(define gtp-repo-url "https://github.com/ndh4/gtp-benchmarks.git")
 (define expected-gtp-branch "configurable-ctcs-module-level")
 (define expected-blgt-branch "blutil")
 (define with-TR+transient? #t)
