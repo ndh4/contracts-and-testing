8f73e6125ebc292d2835d272b8c2168bc51a710a
ndh4 <ndh4@rice.edu>
2025-07-09 14:11:48 -0500

    No sampling on mutants

--- bex/orchestration/db-setup/blutil.rkt
+++ bex/orchestration/db-setup/blutil.rkt
@@ -2,14 +2,14 @@
 
 (require "db-setup.rkt")
 
-(db-setup-script
- #:mutation-analysis-config "../../configurables/blutil-configs/mutation-code-mistake-analysis.rkt"
- #:mutation-analysis-error-type 'any-error
- #:analyze-type-mutation-categories? #f
- #:experiment-config-with-which-analyze-mutants-dynamic-errors "../../configurables/blutil-configs/mutation-code-mistake-analysis.rkt"
- #:dynamic-error-filtering-lattice-config-id 'bot
- #:dynamic-error-interestingness-filter? #f
- #:search-for-interesting-scenarios? #f
- #:mutants-to-sample-per-benchmark 80
- #:no-erasure-mode)
-
+(db-setup-script #:mutation-analysis-config
+                 "../../configurables/blutil-configs/mutation-code-mistake-analysis.rkt"
+                 #:mutation-analysis-error-type 'any-error
+                 #:analyze-type-mutation-categories? #f
+                 #:experiment-config-with-which-analyze-mutants-dynamic-errors
+                 "../../configurables/blutil-configs/mutation-code-mistake-analysis.rkt"
+                 #:dynamic-error-filtering-lattice-config-id 'bot
+                 #:dynamic-error-interestingness-filter? #f
+                 #:search-for-interesting-scenarios? #f
+                 #:mutants-to-sample-per-benchmark 'all
+                 #:no-erasure-mode)
