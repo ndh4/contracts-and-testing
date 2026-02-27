7aa180016c681d51cf19aec6ea36bab7d02a12bf
ndh4 <ndh4@rice.edu>
2026-02-27 09:54:35 -0600

    All 7 benchmarks

--- bex/util/log-controls.rkt
+++ bex/util/log-controls.rkt
@@ -3,9 +3,9 @@
 (provide (all-defined-out))
 
 (define-values (print-mutant-factory-logs? mutant-factory-log-level) (values #t 'info))
-(define-values (print-mutant-util-logs? mutant-util-log-level) (values #f 'info))
+(define-values (print-mutant-util-logs? mutant-util-log-level) (values #f 'debug))
 (define-values (print-mutant-runner-logs? mutant-runner-log-level) (values #f 'info))
 (define-values (print-mutate-logs? mutate-log-level) (values #f 'info))
-(define-values (print-mutation-runner-logs? mutation-runner-log-level) (values #f 'info))
+(define-values (print-mutation-runner-logs? mutation-runner-log-level) (values #f 'warning))
 (define-values (print-instrumented-runner-logs? instrumented-runner-log-level) (values #f 'info))
 (define-values (print-teco-run-logs? teco-run-log-level) (values #f 'info))
