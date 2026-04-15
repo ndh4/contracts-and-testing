bce314ce5a3ce1b7b3c87bb3219bb45577dd0877
Nick Breitling <breitling.nw@gmail.com>
2026-04-15 10:16:12 -0500

    passing mutant sampling phase of db-setup
    Did not actually end up threading the experiment-dir because it was
    unnecessary. Instead, changed configurable so that it won't break if the
    parameter is #f

--- bex/configurables/teco-configs/teco-prime.rkt
+++ bex/configurables/teco-configs/teco-prime.rkt
@@ -4,18 +4,21 @@
 
 (configure! mutation                      code-mistakes)
 (configure! mutant-sampling               pre-selected
-            (build-path (current-experiment-dir) "dbs" "mutant-samples.rktdb"))
+            (and (current-experiment-dir)
+                 (build-path (current-experiment-dir) "dbs" "mutant-samples.rktdb")))
 (configure! mutant-filtering              dont-care-about-blame-trails)
 (configure! module-selection-for-mutation all-regular-modules)
 (configure! benchmark-runner              teco-run) ;; TODO Change this to interpret result possibly
 (configure! blame-translation             configurable-ctc-middleman-mod-to-source)
 (configure! blame-following               null)
 (configure! bt-root-sampling              pre-selected
-            (build-path (current-experiment-dir) "dbs" "pre-selected-bt-roots.rktdb"))
+            (and (current-experiment-dir)
+                 (build-path (current-experiment-dir) "dbs" "pre-selected-bt-roots.rktdb")))
 (configure! trail-completion              any-type-error/blamed-at-max)
 (configure! configurations                teco-configs)
 (configure! sql-data-collection           test-and-mutant-db  
-            (build-path (current-experiment-dir) "db.sqlite"))
+            (and (current-experiment-dir)
+                 (build-path (current-experiment-dir) "experiment-output" "db.sqlite")))
 
 (configure! module-instrumentation    insert-test)
 (configure! program-instrumentation   insert-test-in-main)
