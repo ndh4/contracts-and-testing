0958cb701cd542884c637d34a59071fafc6007d5
Nick Breitling <breitling.nw@gmail.com>
2026-04-15 11:20:59 -0500

    Remove parity check in standard-experiment-runner-script-template
    This was originally a workaround for a typed racket issue and
    is not relevant to our experiment, so it's safe to remove

--- bex/orchestration/standard-experiment-runner-script-template.sh
+++ bex/orchestration/standard-experiment-runner-script-template.sh
@@ -61,7 +61,6 @@ export PLTSTDERR='none'
 # in the experiment runner script
 # TODO sqlite database should not be in db_dir, it should be in output_dir
 OUTPUT_DIR=$EXPERIMENT_DIR/experiment-output/$OUTPUT_DIR_NAME
-DB_DIR=$EXPERIMENT_DIR/dbs
 DATA_DIR=$EXPERIMENT_DIR/mutant-runner-results/$CONFIG_NAME
 BENCHMARKS_PATH=gtp-benchmarks/benchmarks
 
@@ -77,6 +76,5 @@ hostname >> $OUTDIR/$BENCH.log
     -e "$OUTPUT_DIR/errs.log" \
     -c "contracts-and-testing/bex/configurables/configs/$CONFIG_NAME" \
     -m "$OUTPUT_DIR/$BENCH-metadata.rktd" \
-    -P "$DB_DIR/configuration-outcomes/$OUTPUT_DIR_NAME.rkt" \
     $KEEP_GOING_FLAG >> $OUTDIR/$BENCH.log 2>&1
 popd > /dev/null
