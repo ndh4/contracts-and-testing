c906dd8492d5b90c56da53e49f3e1cfd2c1b194d
Nick Breitling <breitling.nw@gmail.com>
2026-04-29 11:15:38 -0500

    Prompt user for experiment-dir cleanup on early-stage failure

--- bex/orchestration/experiment-lang.rkt
+++ bex/orchestration/experiment-lang.rkt
@@ -137,7 +137,8 @@
                                (e.g. with `experiment-manager.rkt`).
                                Done? (No means abort.)
                                })
-      (raise-user-error 'handle-host-update-failure! "Aborted.")))
+    (prompt-for-cleanup!)
+    (raise-user-error 'handle-host-update-failure! "Aborted.")))
 (define ((handle-host-db-setup-failure! experiment-id) msg)
   (unless (help!:continue? msg
                            @~a{
@@ -146,7 +147,8 @@
                                Fix the problem before continuing.
                                Done? (No means abort.)
                                })
-      (raise-user-error 'handle-host-update-failure! "Aborted.")))
+    (prompt-for-cleanup!)
+    (raise-user-error 'handle-host-update-failure! "Aborted.")))
 (define ((handle-launch-benchmarks-failure! experiment-id) benchmark)
   (unless (help!:continue? @~a{Benchmark launch failed}
                            @~a{
@@ -155,6 +157,7 @@
                                Fix the problem and launch the benchmark before continuing.
                                Done? (No means abort.)
                                })
+    (prompt-for-cleanup!)
     (raise-user-error 'handle-launch-benchmarks-failure! "Aborted.")))
 
 (define (handle-job-data-disappeared-failure! host experiment-id)
@@ -164,6 +167,7 @@
                                Continue with the rest of the experiment?
                                If the results are there, download them manually before continuing.
                                })
+    ;; Do not prompt for cleanup here, since this could mean a success. Better safe than sorry!
     (raise-user-error 'handle-job-data-disappeared-failure! "Aborted.")))
 
 (define (run-one-mode host
