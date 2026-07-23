f3bd88d61067efbfe3a3aca51f8bd31560769eb7
ndh4 <ndh4@rice.edu>
2026-07-23 13:44:49 -0500

    Handle case where info-str looks like #hash((completed . ()) (errored . ((abm_test ? ?))) (incomplete . ()) (other . ())) because intro line of log file not printed yet

--- bex/orchestration/experiment-manager.rkt
+++ bex/orchestration/experiment-manager.rkt
@@ -78,7 +78,12 @@
      (error info-str)
      (match info-str
        [(regexp "^#hash")
-        (call-with-input-string info-str read)]
+        (cond
+         [(string-contains? info-str "?")
+          (eprintf @~a{Unable to get experiment results summary for host @a-host, @;
+                       found: @~v[info-str]})
+          absent]
+         [else (call-with-input-string info-str read)])]
        [else
         (eprintf @~a{
                      Unable to get experiment results summary for host @a-host, @;
